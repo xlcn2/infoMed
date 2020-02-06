a869045fd6aadfa51e5da25c90bc4e60257b0805
unknown <lucianopeas@gmail.com>
2020-02-06 13:06:11 -0200

    atualização

--- app/Http/Controllers/MedicoController.php
+++ app/Http/Controllers/MedicoController.php
@@ -7,9 +7,8 @@ use App\Medico;
 use App\Especialidade;
 use DB;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Session;
 use Redirect;
-use Auth;
+
 
 class MedicoController extends Controller
 {
@@ -66,53 +65,62 @@ class MedicoController extends Controller
 	public function procurarMedico($crm, Request $request)
 	{
         $especialidades = Especialidade::where('crm', $crm)->get();
-
 		$medico = $this->medico->find($crm);
 		return view('atualizar', compact('medico','especialidades'));
 
 	}
 
-	public function gerenciador()
-	{
+	public function gerenciador(){
+        //pagina inicial
 		$medicos = Medico::all();
 		$especialidades = Especialidade::all();
 		return view('tabela', compact('medicos','especialidades'));
 	}
+    
+    
+    
+    
+    
 
-	public function atualizarMedico(Request $request, $crm)
-	{
+	public function atualizarMedico(Request $request, $crm){
+        
+        //variavel para verificar o tamanho da lista de especialidades selecionadas 
         $tamanho = count($request->get('especialidades'));
       
-        if($tamanho<2){
+        //se houver menos de uma especialidade redirecionará de volta para a pagina de atualização
+        if($tamanho<1){
            return redirect()->back();
         }
-      //atualizar especializações
+        
         DB::beginTransaction();
         $especialidade = Especialidade::find($crm);
         
       try {
+            //deleta todas as especialidades anteriores cadastradas
             $this->especialidade->where('crm', $crm)->delete();
 			DB::commit();
           
-          
-          $especialidades = $request->get('especialidades');
-        $tamanho = count($request->get('especialidades'));
-        
-       for ($i = 0; $i < $tamanho; $i++) {
-            $especialidade = new Especialidade([
-                 'nome' => $especialidades[$i],
-                 'crm'=> $request->get('crm')]);
-                  $especialidade->save();
-           
-       }
-          
-		} catch (Exception $e) {
-			DB::rollback();
-		}
-        
-        
-        //atualizar medico
+            //recupera a lista de especialidades selecionadas pelo o  usuario
+            $especialidades = $request->get('especialidades');
 
+            
+
+          
+          //cadastra todas as novas ou habituais especialidades selecionadas
+           for ($i = 0; $i < $tamanho; $i++) {
+                $especialidade = new Especialidade([
+                     'nome' => $especialidades[$i],
+                     'crm'=> $request->get('crm')]);
+                      $especialidade->save();
+
+           }
+
+            } catch (Exception $e) {
+                DB::rollback();
+            }
+        
+        
+        //atualiza  os demais dados do medico
 		try {
 			$medico = Medico::find($crm);
 
@@ -121,13 +129,13 @@ class MedicoController extends Controller
 			});
 
 			return redirect('/');
-		} catch (Exception $e) {
+    		} catch (Exception $e) {
 
-		}
+	     	}
 	}
 
-	public function deletarMedico($crm)
-	{
+	   
+    public function deletarMedico($crm){
 		
 		DB::beginTransaction();
 		$medico = Medico::find($crm);
